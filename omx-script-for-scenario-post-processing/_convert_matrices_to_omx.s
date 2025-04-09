8ab50d27e4ac6f91b503d46cffffde082c3d7c31
Bill Hereth <bhereth@wfrc.org>
2025-04-09 11:50:46 -0600

    Move investigations into archive folder

---
+++ omx-script-for-scenario-post-processing/_convert_matrices_to_omx.s
@@ -0,0 +1,13 @@
+CONVERTMAT FROM='4_ModeChoice\2_DetailedTripMatrices\HBW_trips_allsegs_Pk.mtx', TO='4_ModeChoice\2_DetailedTripMatrices\HBW_trips_allsegs_Pk.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\2_DetailedTripMatrices\HBO_trips_allsegs_Pk.mtx', TO='4_ModeChoice\2_DetailedTripMatrices\HBO_trips_allsegs_Pk.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\2_DetailedTripMatrices\NHB_trips_allsegs_Pk.mtx', TO='4_ModeChoice\2_DetailedTripMatrices\NHB_trips_allsegs_Pk.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\2_DetailedTripMatrices\HBC_trips_allsegs_Pk.mtx', TO='4_ModeChoice\2_DetailedTripMatrices\HBC_trips_allsegs_Pk.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\2_DetailedTripMatrices\HBW_trips_allsegs_Ok.mtx', TO='4_ModeChoice\2_DetailedTripMatrices\HBW_trips_allsegs_Ok.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\2_DetailedTripMatrices\HBO_trips_allsegs_Ok.mtx', TO='4_ModeChoice\2_DetailedTripMatrices\HBO_trips_allsegs_Ok.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\2_DetailedTripMatrices\NHB_trips_allsegs_Ok.mtx', TO='4_ModeChoice\2_DetailedTripMatrices\NHB_trips_allsegs_Ok.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\1a_Skims\skm_d8_Pk.mtx', TO='4_ModeChoice\1a_Skims\skm_d8_Pk.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\1a_Skims\skm_d8_Ok.mtx', TO='4_ModeChoice\1a_Skims\skm_d8_Ok.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\1a_Skims\skm_w8_Ok.mtx', TO='4_ModeChoice\1a_Skims\skm_w8_Ok.omx', FORMAT=OMX COMPRESSION=6
+CONVERTMAT FROM='4_ModeChoice\1a_Skims\skm_w8_Pk.mtx', TO='4_ModeChoice\1a_Skims\skm_w8_Pk.omx', FORMAT=OMX COMPRESSION=6
+
+
